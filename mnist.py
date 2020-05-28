6a52687ca7154eea35ed7e6606f8c32419e0c243
500060783 <500060783@stu.upes.ac.in>
2020-05-29 01:40:04 +0530

    Changing code

--- mnist.py
+++ mnist.py
@@ -1,32 +1,68 @@
 from keras.datasets import mnist
 
-dataset=mnist.load_data('mymnist.db')
-train,test=dataset
-X_train,y_train=train
-X_test,y_test=test
-img1=X_train[0]
-img1_label=y_train[0]
-
-import matplotlib.pyplot as plt
-plt.imshow(img1,cmap='gray')
-img1_1d=img1.reshape(28*28)
-
-X_train_1d=X_train.reshape(-1,28*28)
-
-X_train=X_train_1d.astype('float32')
-
 from keras.utils.np_utils import to_categorical
-
-y_train_cat=to_categorical(y_train)
 
 from keras.models import Sequential
 
 from keras.layers import Dense
 
+from keras.optimizers import RMSprop
+
+dataset=mnist.load_data()
+
+train,test=dataset
+X_train,y_train=train
+
+X_test,y_test=test
+
+
+
+X_train=X_train.reshape(-1,28*28)
+
+X_train=X_train.astype('float32')
+
+y_train=to_categorical(y_train)
+
+
+
+
+X_test=X_test.reshape(-1,28*28)
+
+X_test=X_test.astype('float32')
+y_test=to_categorical(y_test)
+
+
+
+accuracy=0
+counter=1
+learning_rate=0.1
+epoch=10
+
+while accuracy < .90 :
+    model = Sequential()
+    for i in range(counter) :
+            model.add(Dense(units=128,activation="relu",input_shape=(784,)))
+      
+    counter = counter +1
+    print("counter is ",counter)
+    
+    model.add(Dense(units=10,activation="softmax"))      
+    learning_rate = learning_rate/10
+    print("learning_rate is", learning_rate)
+    model.compile(optimizer=RMSprop(learning_rate),loss="categorical_crossentropy",metrics=["accuracy"])
+    model.fit(X_train,y_train,batch_size=32,epochs=epoch,verbose=1)
+    model.summary()
+    Accuracy = model.evaluate(x=X_test,y=y_test,batch_size=32)
+    print("Accuracy: ",Accuracy[1])
+    accuracy = Accuracy[1]
+    print(accuracy)
+    
+
+
+'''
 model=Sequential()
 
 model.add(Dense(units=512,input_dim=28*28,activation='relu'))
-
 model.add(Dense(units=256,activation='relu'))
 
 model.add(Dense(units=128,activation='relu'))
@@ -37,23 +73,16 @@ model.add(Dense(units=10,activation='softmax'))
 
 model.summary()
 
-from keras.optimizers import RMSprop
 
 model.compile(optimizer=RMSprop(),loss='categorical_crossentropy',metrics=['accuracy'])
 
 model.fit(X_train,y_train_cat,epochs=3)
 
 accuracy=model.evaluate(x=X_test, y=y_test, batch_size=32)
+
 print("Accuracy",accuracy[1])
+
 Accuracy=accuracy[1]
+
 print(Accuracy)
-
-#plt.imshow(X_test[9999])
-
-#y_test[9999]
-
-#test_img=X_test[9999].reshape(-1,28*28)
-
-#test_img=test_img.astype('float32')
-
-#model.predict(test_img)
+'''
